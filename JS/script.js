eb5b739dc56dfc9eaf05d906f0bc191f58635fe9
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-08-21 01:33:01 +0200

    added event listener to the prev & next btn

--- JS/script.js
+++ JS/script.js
@@ -25,24 +25,61 @@ const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'im
 
 //2. Recupero gli elementi di'interesse all'interno del DOM.
 const galleryImgs = document.getElementById('gallery');
-
 const prevButton = document.getElementById('prev');
-
 const nextButton = document.getElementById('next');
-
 
 /* fase di elaborazione */
 
 //3. Generero le img da inserire all'interno del DOM, con il loop For.
 
-
 for (let i = 0; i < sources.length; i++) {
     const src = sources[i];
+    const img = document.createElement('img');
 
-    const images = document.createElement('img');
-    images.src = src;
-    images.alt = `0${i + 1}.web`
-
-    galleryImgs.appendChild(images)
+    img.src = src;
+    img.alt = `0${i + 1}.web`;
+    galleryImgs.appendChild(img);
 
 }
+
+const images = document.querySelectorAll('#gallery img');
+console.log(images)
+
+//4. Aggiungo la classe active all'img in mostra.
+let currentActiveImage = 0;
+images[currentActiveImage].classList.add('active')
+
+
+
+/* Gestione degli eventi */
+
+/*5.Al click del BTN-next aggiungere la classe active all'img succesiva 
+    e toglierlo a quella corrente.*/
+
+nextButton.addEventListener('click', function () {
+    images[currentActiveImage].classList.remove('active');
+
+    //5.b incremento del indice per poter dare la classe active al img succesiva.
+    currentActiveImage++;
+
+    //5.c aggiungere la classe active all'image sucessiva.
+    images[currentActiveImage].classList.add('active')
+
+})
+
+
+/*6.Al click del BTN-prev aggiungere la classe active all'img precedente
+ e toglierlo a quella corrente.*/
+
+
+prevButton.addEventListener('click', function () {
+    images[currentActiveImage].classList.remove('active');
+
+    //6.b incremento del indice per poter dare la classe active al img succesiva.
+    currentActiveImage--;
+
+    //6.c aggiungere la classe active all'image sucessiva.
+    images[currentActiveImage].classList.add('active')
+
+})
+
