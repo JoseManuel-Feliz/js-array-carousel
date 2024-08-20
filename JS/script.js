f9742393e933b6ed75f81b6d3f53d81f4180f311
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-20 23:56:14 +0200

    added JS steps and removed img inside the dom

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+console.log('JS OK')
+
+
+/* 
+STEPS
+
+1.Creazione di una lista di array con le source delle img.
+
+2. Recupero gli elementi di'interesse all'interno del DOM.
+
+3. Generero le img da inserire all'interno del DOM, con il loop For.
+
+4. Aggiungo la classe active all'img in mostra.
+
+5.Al click del BTN next aggiungere la classe active all'img succesiva e toglierlo a quella corrente.
+
+6.Al click del BTN next aggiungere la classe active all'img precedente e toglierlo a quella corrente.
+
+
+*/
