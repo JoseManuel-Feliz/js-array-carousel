6985414b33ff96c48ec7b0a4464da3bcff5a32cd
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-08-21 00:30:01 +0200

    added  imgs loop for and placed inside the dom

--- JS/script.js
+++ JS/script.js
@@ -21,11 +21,28 @@ STEPS
 /* Fase di preparazione */
 
 //1.Creazione di una lista di array con le source delle img.
-const sources = ['img/01.webp', 'img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 
 //2. Recupero gli elementi di'interesse all'interno del DOM.
 const galleryImgs = document.getElementById('gallery');
 
-const prevButton = documet.getElementById('prev');
+const prevButton = document.getElementById('prev');
 
-const nextButton = documet.getElementById('next');
+const nextButton = document.getElementById('next');
+
+
+/* fase di elaborazione */
+
+//3. Generero le img da inserire all'interno del DOM, con il loop For.
+
+
+for (let i = 0; i < sources.length; i++) {
+    const src = sources[i];
+
+    const images = document.createElement('img');
+    images.src = src;
+    images.alt = `0${i + 1}.web`
+
+    galleryImgs.appendChild(images)
+
+}
