537fba7b2247b41d788b8317932372a68d59a3db
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-08-21 00:04:03 +0200

    added step 1 & 2 JS

--- JS/script.js
+++ JS/script.js
@@ -16,5 +16,16 @@ STEPS
 
 6.Al click del BTN next aggiungere la classe active all'img precedente e toglierlo a quella corrente.
 
-
 */
+
+/* Fase di preparazione */
+
+//1.Creazione di una lista di array con le source delle img.
+const sources = ['img/01.webp', 'img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+
+//2. Recupero gli elementi di'interesse all'interno del DOM.
+const galleryImgs = document.getElementById('gallery');
+
+const prevButton = documet.getElementById('prev');
+
+const nextButton = documet.getElementById('next');
